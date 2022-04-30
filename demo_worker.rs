023cea64fb49162e9000ff3950516dbe4d2b14b2
serch <sergixx31@gmail.com>
2022-04-30 10:49:53 +0200

    hola carlos jeje

--- demo_worker.rs
+++ demo_worker.rs
@@ -1,10 +1,9 @@
-
 for w in map.workers {
     // Logic to check worker placement
 }
 
-for x in 0..40 {
-    for y in 0..40 {
+for x in 0..50 {
+    for y in 0..50 {
         if map[x][y] == Tile::EMPTY {
             // more logic
         }
@@ -12,14 +11,25 @@ for x in 0..40 {
     }
 }
 
-for w in 0..8 {
-    let r = (rand() % 4).abs();
-    switch r {
-        0 => worker(w).move_up(),
-        1 => worker(w).move_down(),
-        2 => worker(w).move_right(),
-        3 => worker(w).move_left(),
-    }
+//for w in 0..1 {
+//    let r = (rand() % 4).abs();
+//    switch r {
+//        0 => worker(w).move_up(),
+//        1 => worker(w).move_down(),
+//        2 => worker(w).move_right(),
+//        3 => worker(w).move_left(),
+//    }
+for z in 0..8 {
 
-    info(`worker ${w} finished`);
+    worker(z).move_down();
+    info(`pos ${worker(0).x}, ${worker(0).y}`);
+    if worker(z).y == 10{
+        worker(z).move_left();
+    if worker(z).x == 5{
+        worker(z).move_up();
 }
+
+    }
+  }
+
+
